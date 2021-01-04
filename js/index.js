efae92013e33aa5abaeb616ab152a8a05b0ce1b0
jwong209 <68797448+jwong209@users.noreply.github.com>
2021-01-04 15:05:50 -0800

    just added

---
+++ js/index.js
@@ -0,0 +1,53 @@
+console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function() {
+    
+   
+    $('.hidemex').slideDown(1500);
+    $('.hidexem').slideDown(1500);
+
+      /* Every time the window is scrolled ... */
+      $(window).scroll( function(){
+      
+          /* Check the location of each desired element */
+          $('.hideme').each( function(i){
+              
+              var bottom_of_object = $(this).position().top + $(this).outerHeight();
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+              /* If the object is completely visible in the window, fade it it */
+              if( bottom_of_window > bottom_of_object ){
+                  
+                  $(this).animate({'opacity':'1'},1000);
+                      
+                }
+            }); 
+        });
+});
+
+
+//Get the button:
+var mybutton = document.getElementById("top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+ $("#top").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+   });
+
+  
